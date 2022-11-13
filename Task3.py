18b500144508fe0c4b686b8f6f5e8067eefb7352
caseynv <nwandukelechi@gmail.com>
2022-11-13 01:56:45 +0100

    updates

--- Task3.py
+++ Task3.py
@@ -131,79 +131,6 @@ def get_metadata_item(title, lang) -> str:
             
 #get_metadata_item('File:Açude_Cedro_-_Detalhe_do_acabamento_da_barragem_principal.jpg', lang='commons')
 
-def get_summary_data(title, lang) -> str:
-    
-    """
-    summary data of te file on the homepage using api: https://commons.wikimedia.org/w/api.php
-
-    Args:
-        title (string): the title of the commons file
-        lang(string):+  the particular wikipedia api needed eg en, fr, commons
-
-    Returns:
-        DateTimeOriginal|Categories|License|LicenseUrl|ImageDescription|Credit|GPSLatitude|GPSLongitude
-    """
-    
-    session = mwapi.Session(
-        host="https://{0}.wikimedia.org/w/api.php".format(lang),
-        user_agent="Outreachy round fall 2022"
-    )
-
-    params = {
-            "action": "query",
-            "prop":"imageinfo",
-            "titles": title,
-            "iiprop":"extmetadata",
-            "format": "json",
-    }
-
-    response = session.get(params) #get request for the wikiapi
-    response_pages = response['query']['pages'] 
-    page_id = list(response_pages.keys())[0]  # gets the pageid
-    imageinfo = response_pages[page_id]['imageinfo']
-    
-    for response_item in imageinfo:
-        response_metadata = response_item['extmetadata']
-        
-        print(title)
-        
-        print("DateTimeOriginal", '-> ', response_metadata["DateTimeOriginal"]['value'])
-        print("DateTime", '-> ', response_metadata["DateTime"]['value'])
-        print('Categories', '-> ', response_metadata['Categories']['value'])
-        print('License', '-> ', response_metadata['License']['value'])
-        
-        try:  #the artist difer for different artist some are embedded in a link others are not
-            artist_value = re.findall(r"<a\b[^>]*>([^<]+)<\/a>", response_metadata["Artist"]['value']) # regex to extract the artist name from anchor html tags
-            print("Artist", '-> ', artist_value[0])
-        except:
-            artist_value = re.findall(r"<span[^>]*>(.*?)</span>", response_metadata["Artist"]['value']) # regex to extract the image description from anchor html tags
-            print("Artist", '-> ', artist_value[0])
-        
-        try: #the license url is absent for some files in commons
-            print('LicenseUrl', '-> ', response_metadata['LicenseUrl']['value'])
-        except:
-            continue
-            
-        try: #the credit value difer for different artist some are embedded in a link others are not
-            credit_value = re.findall(r"<span\b[^>]*>([^<]+)<\/span>", response_metadata["Credit"]['value']) # regex to extract the credit from span html tags
-            print("Credit", "-> ", credit_value[0])
-        except:
-            print("Credit", "-> ", response_metadata["Credit"]['value'])
-            
-        try: #the imagedescription difer for different artist some are embedded in a link others are not
-            image_value = re.findall(r"(.*?)<a\b[^>]*>([^<]+)<\/a>", response_metadata["ImageDescription"]['value']) # regex to extract the image description from anchor html tags
-            print("ImageDescription", "-> ", image_value[0][0], image_value[0][1])
-        except:
-            print("ImageDescription", "-> ", response_metadata["ImageDescription"]['value'])
-        
-        try: #some files do not have camera location
-            print("GPSLatitude", '-> ', response_metadata["GPSLatitude"]['value'])
-            print("GPSLongitude", '-> ', response_metadata["GPSLongitude"]['value'])
-        except KeyError:
-            continue 
-        
-        
-#get_summary_data('File:Açude_Cedro_-_Detalhe_do_acabamento_da_barragem_principal.jpg', lang='commons')
 
 def get_all_files_data(title, lang) -> str:
     
@@ -254,26 +181,26 @@ def get_all_files_data(title, lang) -> str:
                     if response_itemkeys == 'ImageDescription': #conditional statement if the dictionary value is a ImageDescription inorder to use this particular regex to extract image description
                         try: #the imagedescription difer for different artist
                             image_value = re.findall(r"(.*?)<a\b[^>]*>([^<]+)<\/a>", response_item[response_item_keys][response_itemkeys]['value']) # regex to extract the image description from anchor html tags
-                            print("ImageDescription", "-> ", image_value[0][0], image_value[0][1])
+                            print(response_itemkeys, "-> ", image_value[0][0], image_value[0][1])
 
                         except:
-                            print("ImageDescription", "-> ", response_item[response_item_keys][response_itemkeys]['value'])
+                            print(response_itemkeys, "-> ", response_item[response_item_keys][response_itemkeys]['value'])
                             
                     elif response_itemkeys == 'Artist': #conditional statement if the dictionary value is an artist inorder to use this particular regex to extract artist name
                         try: #the artist difer for different artist
                             artist_value = re.findall(r"<a\b[^>]*>([^<]+)<\/a>", response_item[response_item_keys][response_itemkeys]['value']) # regex to extract the artist from anchor html tags
-                            print("Artist", '-> ', artist_value[0])
+                            print(response_itemkeys, '-> ', artist_value[0])
                         except:
                             artist_value = re.findall(r"<span\b[^>]*>([^<]+)<\/span>", response_item[response_item_keys][response_itemkeys]['value']) # regex to extract the artist from anchor html tags
-                            print("Artist", '-> ', artist_value[0])
+                            print(response_itemkeys, '-> ', artist_value[0])
                     
                     elif response_itemkeys == 'Credit': #conditional statement if the dictionary value is a credit property inorder to use this particular regex to extract credit
                         try: #the credit difer for different artist
                             credit_value = re.findall(r"<span\b[^>]*>([^<]+)<\/span>", response_item[response_item_keys][response_itemkeys]['value']) # regex to extract the credit from span html tags
-                            print("Credit", "-> ", credit_value[0])
+                            print(response_itemkeys, "-> ", credit_value[0])
                         except:
                             credit_value = re.findall(r"(.*?)<a\b[^>]*>([^<]+)<\/a>", response_item[response_item_keys][response_itemkeys]['value']) # regex to extract the credit from span html tags
-                            print("Credit", "-> ", credit_value[0][0], credit_value[0][1])
+                            print(response_itemkeys, "-> ", credit_value[0][0], credit_value[0][1])
                             
                     else:
                         print(response_itemkeys, ' -> ', response_item[response_item_keys][response_itemkeys]['value'])
@@ -305,17 +232,15 @@ def get_all_files_subcat(cat_title, lang='commons') -> list:
                 "generator":"categorymembers", #Get information about all categories used in the page
                 "gcmlimit": 500,
                 "gcmtitle": cat_title,
-                "gcmnamespace": 6,
+                "gcmnamespace": 6, #the namespace here means it gets just files, but a 14 gets a subcategory
                 "format": "json",
         }
     
     response = session.get(params_1)
     pageids = list(response['query']['pages'].keys())
     
-    files_list=[]
-    for pageid in pageids:
-        files = response['query']['pages'][str(pageid)]['title']
-        files_list.append(files)
+    files_list=[response['query']['pages'][str(pageid)]['title'] for pageid in pageids]
+    
     return files_list
 
 #get_all_files_subcat('Category:2021 in São Paulo (state)', lang='commons')
@@ -349,12 +274,11 @@ def get_all_files_cat(cat, lang):
     response = session.get(params) #get request for the wikiapi to get the category members pageid
     category_member = response['query']['categorymembers']
     
-    cat_file_list=[]
-    for category_item in category_member:
+    cat_file_list= sum([get_all_files_subcat(category_item['title'], lang='commons') for category_item in category_member], []) #add more files to the list to create the category list
+    '''for category_item in category_member:
         cat_title = category_item['title']
         files = get_all_files_subcat(cat_title, lang='commons')
-        #print(cat_title, ' -> ', files)
-        cat_file_list += files
+        cat_file_list += files '''
         
     return cat_file_list
 
@@ -383,7 +307,7 @@ def get_wikidata(cat, lang) -> list:
             "generator":"categorymembers", #Get information about all categories used in the page
             "gcmlimit": 500,
             "gcmtitle": cat,
-            "gcmnamespace": 6,
+            "gcmnamespace": 6, #get files as namespace 6 is for files note it can also be used to get subcategories 
             "format": "json",
     }
 
@@ -410,35 +334,21 @@ def get_wikidata(cat, lang) -> list:
                 try:
                     wikidata_num = property_depict[dict_place]['mainsnak']['datavalue']['value']['id'] #get the wikidata number 
 
-                    params_2 = {
-                        "action": "wbgetentities", #Gets the data for multiple Wikibase entities.
-                        "ids": str(wikidata_num),
-                        "props": "labels",
-                        "languages": "en",
-                        "format": "json",
-                    }
-
-                    response_2 = session.get(params_2) #get request for the wikiapi to get information about the wikidata number got
-                    depicts = response_2.get('entities', 'XX').get(wikidata_num).get('labels', 'XX').get('en', 'XX') #The names of the properties to get back from each entity. Will be further filtered by any languages given.
-                    
-                    if "XX" not in str(depicts):
-                        info = str(depicts['value']) + " (" + str(wikidata_num) + ")" #gets the depict value and their respective wikidata number
-                        #depictslist.append(info)
-                        wikidata_list.append(wikidata_num)
+                    wikidata_list.append(wikidata_num)
                         
                 except:
                     continue 
-            else:
-                continue
+            
                 
         else:
             continue
             
-    new_wikidata_list=[]
-    for list_item in wikidata_list:
+    new_wikidata_list=list(set([list_item for list_item in wikidata_list])) #further ensure the wikidata numbers in the list are unique
+    
+    '''for list_item in wikidata_list:
         
-        if list_item not in new_wikidata_list: #further ensure the wikidata numbers in the list are unique
-            new_wikidata_list.append(list_item)
+        if list_item not in new_wikidata_list: 
+            new_wikidata_list.append()'''
             
     return new_wikidata_list
             
@@ -478,39 +388,41 @@ def get_labels_description_subcat(cat, lang):
 
     for category_item in category_member:
         
-        wikidata_list = get_wikidata(category_item['title'], lang='commons')
-        print('\n', category_item['title'])
-        
-        for wikidata_item in wikidata_list:
+        try:
+            wikidata_list = get_wikidata(category_item['title'], lang='commons')
+            print('\n', category_item['title'])
             
-            sparql = """
-            SELECT 
-                 ?item ?itemLabel ?itemDescription 
-                  ?locationLabel ?locationDescription  
-                  ?streetLabel  
-                  ?descriptionLabel 
-                  ?heritageLabel
-                WHERE {
-                  VALUES ?item { wd:"""+wikidata_item+""" }
-                  ?item wdt:P131 ?location ;
-                        wdt:P1435 ?heritage .
-                  SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
-                  OPTIONAL {?item 
-                        wdt:P6375 ?street ;
-                        wdt:P973 ?description ;}
-                }
-            """
-            
-            wikiquery = SparqlQuery() #sparqlquery that allows the use of sparql queries with python
-            response = wikiquery.query(sparql)
-            results = response['results']['bindings'] #get list of all the response results
-            
-            for response_item in results:
+            for wikidata_item in wikidata_list:
                 
-                for response_keys in list(response_item.keys()):
-                    print(response_keys, ' -> ', response_item[response_keys]['value'])
-
+                sparql = """
+                SELECT 
+                    ?item ?itemLabel ?itemDescription 
+                    ?locationLabel ?locationDescription  
+                    ?streetLabel  
+                    ?descriptionLabel 
+                    ?heritageLabel
+                    WHERE {
+                    VALUES ?item { wd:"""+wikidata_item+""" }
+                    ?item wdt:P131 ?location ;
+                            wdt:P1435 ?heritage .
+                    SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
+                    OPTIONAL {?item 
+                            wdt:P6375 ?street ;
+                            wdt:P973 ?description ;}
+                    }
+                """
             
+                wikiquery = SparqlQuery() #sparqlquery that allows the use of sparql queries with python
+                response = wikiquery.query(sparql)
+                results = response['results']['bindings'] #get list of all the response results
+                
+                for response_item in results:
+                    
+                    for response_keys in list(response_item.keys()):
+                        print(response_keys, ' -> ', response_item[response_keys]['value'])
+        except:
+            continue
+                
 #get_labels_description_subcat('Category:Top_contributors_of_Wiki_Loves_Monuments_2020_in_Brazil', lang='commons')
 
 # list of commons files selected
@@ -537,13 +449,6 @@ for title in files_list: #loops through the list of commons files
     get_all_files_data(title, lang='commons')
     print('\n')
 
-# list of commons files selected
-
-files_list = get_all_files_subcat('Category:Images_by_Ana_Beatriz_Sampaio_in_Wiki_Loves_Monuments_2021_in_Brazil', lang='commons')
-
-for title in files_list: #loops through the list of commons files
-    get_summary_data(title, lang='commons')
-    print('\n')
 
 # list of commons files selected
 
@@ -559,11 +464,11 @@ files_list = get_all_files_subcat('Category:Images_by_Ana_Beatriz_Sampaio_in_Wik
 
 for title in files_list:  #loops through the list of commons files
     
-    hidden_list = get_hidden_categories_list(title, lang='commons')
-    non_hidden_list = get_categories_list(title, lang='commons')
-    all_categories_list = non_hidden_list + hidden_list #get all the categories that is, the hidden plus the unhidden categories
+    all_categories_list = get_categories_list(title, lang='commons') + get_hidden_categories_list(title, lang='commons') #get all the categories that is, the hidden plus the unhidden categories
     print(all_categories_list)
     print('\n')
+
+# list of commons files selected
 
 files_list = get_all_files_subcat('Category:Images_by_Ana_Beatriz_Sampaio_in_Wiki_Loves_Monuments_2021_in_Brazil', lang='commons')
 
@@ -572,11 +477,8 @@ for title in files_list:  #loops through the list of commons files
     all_categories_list = get_categories_list(title, lang='commons') + get_hidden_categories_list(title, lang='commons') #get all the categories that is, the hidden plus the unhidden categories
     all_category += all_categories_list
     
-new_all_category_list=[]
-for list_item in all_category: #list got from the addition of all the hidden and unhidden category list is filtered to remove category repetition
-        
-    if list_item not in new_all_category_list: #further ensure the categories in the list are unique to avoid repetition
-        new_all_category_list.append(list_item)
+new_all_category_list = list(set([list_item for list_item in all_category])) #list got from the addition of all the hidden and unhidden category list is filtered to remove category repetition and further ensure the categories in the list are unique to avoid repetition
+
 for cat in all_categories_list: #looping through the list
     
     if 'Category:Pages with maps' not in cat: #category:pages with maps, amongst others have no entities so this filters it out
@@ -591,9 +493,16 @@ print('\n', info)
 
 #Thoughts
 '''
-The function get_all_files_subcat gives a list of files in a subcategory while get_all_files_cat gives a list of all files in all the subcategory present in the category. This is to prevent hard coding any file name or subcategory. The function get_all_files_cat still goes through further filter the list to ensure no repetition of the wikidata id. Also the all_category list got from the addition of all the hidden and unhidden category list is filtered to remove category repetition.
+The function get_all_files_subcat gives a list of files in a subcategory while get_all_files_cat 
+gives a list of all files in all the subcategory present in the category. This is to prevent hard coding 
+any file name or subcategory. The function get_all_files_cat still goes through further filter the list to 
+ensure no repetition of the wikidata id. Also the all_category list got from the addition of all the hidden 
+and unhidden category list is filtered to remove category repetition.
 
-All these functions ensures that we get all the details from a category/files with or without subcategory. The function get_labels_description_subcat is used to extract information present in the cultural property which cannot be got from the mediawiki api (iwlink gives the wikidata id) as well as the wikidata label and description and other necessary information that might be of use.
+All these functions ensures that we get all the details from a category/files with or without subcategory. 
+The function get_labels_description_subcat is used to extract information present in the cultural property 
+which cannot be got from the mediawiki api (iwlink gives the wikidata id) as well as the wikidata label and 
+description and other necessary information that might be of use.
 '''
 
 
